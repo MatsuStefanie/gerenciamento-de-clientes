54c7174bc5c6d01bc955547358458bd8710b3d79
Stefanie Silva <matsustefanie@gmail.com>
2024-01-29 15:19:24 -0300

    Start project

---
+++ src/test/java/com/matsu/gerenciamentodeclientes/GerenciamentoDeClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matsu.gerenciamentodeclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeClientesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
